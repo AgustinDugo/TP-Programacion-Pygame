24ce201d8c14ce7a6e7cac6eca4aaa61126f9cd7
AgustinDugo <agustin.dugo13@gmail.com>
2025-07-08 21:12:42 -0300

    Se cambia logica para el disparo

--- juego.py
+++ juego.py
@@ -100,10 +100,10 @@ def iniciar_juego():
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 jugando = False
-
+            if evento.type == pygame.KEYDOWN and evento.key == pygame.K_SPACE:
+                disparar(personaje, disparos)
         teclas = pygame.key.get_pressed()
         mover_personaje(teclas, personaje, ANCHO, ALTO)
-        disparar(teclas, personaje, disparos)
         disparos = mover_disparos(disparos)
         enemigos, contador_spawn = generar_enemigos(enemigos, contador_spawn, ANCHO)
         enemigos = mover_enemigos(enemigos, ALTO)
@@ -130,61 +130,8 @@ def iniciar_juego():
         if vidas <= 0:
             jugando = False
             game_over(pantalla, puntaje)
-            return #mostrar_menu() Agregar funcion (mateo)
             
         pygame.display.flip()
 
-import os
-import json
 
-ARCHIVO_RANKING = "ranking.json"
 
-def guardar_puntaje(nombre, puntaje):
-    # Si el archivo no existe, lo crea con una lista vacía
-    if not os.path.exists(ARCHIVO_RANKING):
-        with open(ARCHIVO_RANKING, "w") as archivo:
-            json.dump([], archivo)
-
-    ranking = []
-    if os.path.getsize(ARCHIVO_RANKING) > 0:
-        with open(ARCHIVO_RANKING, "r") as archivo:
-            ranking = json.load(archivo)
-
-    ranking.append({"nombre": nombre, "puntaje": puntaje})
-
-    with open(ARCHIVO_RANKING, "w") as archivo:
-        json.dump(ranking, archivo, indent=4)
-ARCHIVO_RANKING = "ranking.json"
-
-def ordenar_ranking():
-    if not os.path.exists(ARCHIVO_RANKING) or os.path.getsize(ARCHIVO_RANKING) == 0:
-        return []
-
-    with open(ARCHIVO_RANKING, "r") as archivo:
-        ranking = json.load(archivo)
-
-    for i in range(len(ranking)):
-        for j in range(0, len(ranking) - i - 1):
-            if ranking[j]["puntaje"] < ranking[j + 1]["puntaje"]:
-                ranking[j], ranking[j + 1] = ranking[j + 1], ranking[j]
-
-    return ranking
-
-def mostrar_ranking(pantalla):
-    fondo_ranking_img = pygame.image.load("imagenes/eternauta8.jpg").convert()
-    fondo_ranking_img = pygame.transform.scale(fondo_ranking_img, (ANCHO_VENTANA, ALTO_VENTANA))
-
-    pantalla.blit(fondo_ranking_img, (0, 0))
-
-    ranking = ordenar_ranking()
-    fuente_chica = pygame.font.SysFont("Courier New", 40)
-
-    i = 0
-    for jugador in ranking[:5]:
-        texto = fuente_chica.render(f"{i+1}. {jugador['nombre']} - {jugador['puntaje']}", True, COLOR_BLANCO)
-        pantalla.blit(texto, (ANCHO_VENTANA // 2 - 150, 100 + i * 40))
-        i += 1
-    pygame.display.flip()
-    pygame.time.wait(5000)  
-
-iniciar_juego()
